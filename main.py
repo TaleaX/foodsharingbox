6385406393ea4814bafbe692cc3caac36689c038
TaleaX <94018949+TaleaX@users.noreply.github.com>
2022-11-19 00:04:05 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
+def home():
     img = os.path.join('static', 'image.jpg')
     style_path = os.path.join('static', 'style.css')
     return render_template("index.html", user_image = img, style = style_path)
