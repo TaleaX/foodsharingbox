7cfb7cb827168ce06b26a9840913a8b539e7fc87
TaleaX <94018949+TaleaX@users.noreply.github.com>
2022-11-19 00:04:53 +0100

    Update main.py
    well

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from flask import Flask, render_template
 import os
 
 app = Flask(__name__)
+img_path = os.path.join('static', 'image.jpg')
+style_path = os.path.join('static', 'style.css')
 
 @app.route("/")
 def home():
-    img = os.path.join('static', 'image.jpg')
-    style_path = os.path.join('static', 'style.css')
     return render_template("index.html", user_image = img, style = style_path)
 
 if __name__ == "__main__":
