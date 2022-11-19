f1d4b9e6c8deb3a9ac9ea210f6b129698f33c18a
max-schlenz <mschlenz@student.42heilbronn.de>
2022-11-19 10:12:04 +0100

    implement flask_cache_buster

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 from flask import Flask, render_template
+from flask import Flask, url_for
+from flask_cache_buster import CacheBuster
 import os
 
 app = Flask(__name__)
-img_path = os.path.join('static', 'image.jpg')
-style_path = os.path.join('static', 'style.css')
+
+config = {
+     'extensions': ['.jpg', '.css', '.csv'],
+     'hash_size': 10
+}
+
+cache_buster = CacheBuster(config=config)
+cache_buster.register_cache_buster(app)
 
 @app.route("/")
 def home():
-    return render_template("index.html", user_image = img_path, style = style_path)
+    return render_template("index.html", user_image = url_for('static', filename='image.jpg'), style = url_for('static', filename='style.css'))
 
 if __name__ == "__main__":
     #make_picture()
